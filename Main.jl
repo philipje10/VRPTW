97dd06813d0170fc079efca63da871e0a8c40d5a
philipje10 <philipbeelen@hotmail.com>
2020-04-16 20:25:37 +0200

    Changed Main to VRPTW

--- Main.jl
+++
@@ -1,7 +0,0 @@
-include("Filereader.jl")
-include("InitialSolution.jl")
-include("HelpFunctions.jl")
-
-instance = "data/C1_2_1.TXT"
-
-customerPlan, vehiclePlan, unvisitedCustomers = InitialSolutionBuilder(instance,5,5)
---
+++ VRPTW.jl
@@ -0,0 +1,14 @@
+include("Filereader.jl")
+include("InitialSolution.jl")
+include("HelpFunctions.jl")
+
+instance = "data/C1_2_1.TXT"
+
+# customerPlan, vehiclePlan, unvisitedCustomers = InitialSolutionBuilder(instance,5,5)
+
+
+println("this is the first line")
+println("-----------------------")
+println("this is the second line")
+println("this is the third line")
+println("-----------------------")
