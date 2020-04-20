adce8caebf88f904dcae21933c35764c6d184258
philipje10 <philipbeelen@hotmail.com>
2020-04-20 17:56:47 +0200

    Split SolutionChecker, add 2Opt check

--- HelpFunctions.jl
+++ HelpFunctions.jl
@@ -1,41 +1,47 @@
 using Luxor
 
+function RouteCheck(Route,Q,depotCoordinates,depotTimes,customerCoordinates,customerDemand,customerTimes,distDepot,distCustomers)
+    routeFeasibility = true
+    if Route[1] > Q    # capacity check
+        routeFeasibility = routeFeasibility * false
+    end
+    for i = 2:length(Route[2])
+        departLocation = Route[2][i-1]
+        arriveLocation = Route[2][i]
+        if departLocation == 0
+            departTime = depotTimes[1]
+        else
+            departTime = customerPlan[departLocation][2][3]
+        end
+        if arriveLocation == 0
+            arriveTime = departTime + Distance(departLocation,arriveLocation,distDepot,distCustomers)
+            serviceTime = departTime + Distance(departLocation,arriveLocation,distDepot,distCustomers)
+            earliestTime = depotTimes[1]
+            latestTime = depotTimes[2]
+        else
+            arriveTime = customerPlan[arriveLocation][2][1]
+            serviceTime = customerPlan[arriveLocation][2][2]
+            earliestTime = customerTimes[arriveLocation,1]
+            latestTime = customerTimes[arriveLocation,2]
+        end # respected travel time
+        if isapprox((arriveTime - departTime) - Distance(departLocation,arriveLocation,distDepot,distCustomers),0,atol = 1e-4) == false && (arriveTime - departTime) < Distance(departLocation,arriveLocation,distDepot,distCustomers)
+            routeFeasibility = routeFeasibility * false
+        end
+        if serviceTime < earliestTime || serviceTime > latestTime # service within time window
+            routeFeasibility = routeFeasibility * false
+        end
+    end
+    return routeFeasibility
+end
+
 function SolutionCheck(customerPlan,vehiclePlan,unvisitedCustomers,instance)
     ~,Q,~,depotCoordinates,depotTimes,customerCoordinates,customerDemand,customerTimes,~ = ReadInstance(instance)
     distDepot,distCustomers = DistanceMatrix(depotCoordinates,customerCoordinates)
 
     feasibleRoute = Bool[true for r = 1:CurrentVehicle(vehiclePlan)]
     for r = 1:CurrentVehicle(vehiclePlan)
-        if vehiclePlan[r][1] > Q    # capacity check
-            feasibleRoute[r] = feasibleRoute[r] * false
-        end
-        tmp = true
-        for i = 2:length(vehiclePlan[r][2])
-            departLocation = vehiclePlan[r][2][i-1]
-            arriveLocation = vehiclePlan[r][2][i]
-            if departLocation == 0
-                departTime = depotTimes[1]
-            else
-                departTime = customerPlan[departLocation][2][3]
-            end
-            if arriveLocation == 0
-                arriveTime = departTime + Distance(departLocation,arriveLocation,distDepot,distCustomers)
-                serviceTime = departTime + Distance(departLocation,arriveLocation,distDepot,distCustomers)
-                earliestTime = depotTimes[1]
-                latestTime = depotTimes[2]
-            else
-                arriveTime = customerPlan[arriveLocation][2][1]
-                serviceTime = customerPlan[arriveLocation][2][2]
-                earliestTime = customerTimes[arriveLocation,1]
-                latestTime = customerTimes[arriveLocation,2]
-            end # respected travel time
-            if isapprox((arriveTime - departTime) - Distance(departLocation,arriveLocation,distDepot,distCustomers),0,atol = 1e-4) == false && (arriveTime - departTime) < Distance(departLocation,arriveLocation,distDepot,distCustomers)
-                feasibleRoute[r] = feasibleRoute[r] * false
-            end
-            if serviceTime < earliestTime || serviceTime > latestTime # service within time window
-                feasibleRoute[r] = feasibleRoute[r] * false
-            end
-        end
+        Route = vehiclePlan[r]
+        feasibleRoute[r] = RouteCheck(Route,Q,depotCoordinates,depotTimes,customerCoordinates,customerDemand,customerTimes,distDepot,distCustomers)
     end
     if ((count(x -> x = true,feasibleRoute) == CurrentVehicle(vehiclePlan)) && (length(unvisitedCustomers) == 0))
         print("The solution is feasible")
--- VRPTW.jl
+++ VRPTW.jl
@@ -4,7 +4,46 @@ include("HelpFunctions.jl")
 
 instance = "data/C1_2_1.TXT"
 
-customerPlan, vehiclePlan, unvisitedCustomers = InitialSolutionBuilder(instance,5,5)
+K,Q,C,depotCoordinates,depotTimes,customerCoordinates,customerDemand,customerTimes,s = ReadInstance(instance)
+
+customerPlan, vehiclePlan, unvisitedCustomers = InitialSolutionBuilder(instance,1,1)
 SolutionCheck(customerPlan,vehiclePlan,unvisitedCustomers,instance)
 
-PlotSolution(vehiclePlan,160,10,instance)
+# PlotSolution(vehiclePlan,160,10,instance)
+
+# 2-opt*
+
+function TwoOptSwitch(i,j,Q,customerPlan,vehiclePlan,customerDemand)
+    vehicleA = customerPlan[i][1]
+    vehicleB = customerPlan[j][1]
+    oldRouteA = vehiclePlan[vehicleA][2]
+    oldRouteB = vehiclePlan[vehicleB][2]
+    cutPointA = 1
+    cutPointB = 1
+    c = nothing
+
+    while oldRouteA[cutPointA] != i
+        cutPointA += 1
+    end
+
+    while oldRouteB[cutPointB] != j
+        cutPointB += 1
+    end
+
+    newRouteA = vcat(oldRouteA[1:cutPointA], oldRouteB[cutPointB:end])
+    newRouteB = vcat(oldRouteB[1:cutPointB-1], oldRouteA[cutPointA+1:end])
+    capacityA = sum(customerDemand[i] for i in newRouteA[2:end-1])
+    capacityB = sum(customerDemand[i] for i in newRouteB[2:end-1])
+
+    if capacityA > Q || capacityB > Q
+        return false,false
+    else
+        return [Float32(capacityA),Int32[newRouteA]],[Float32(capacityB),Int32[newRouteB]]
+    end
+end
+
+
+i = 43
+j = 64 # (j+1)
+
+A,B = TwoOptSwitch(i,j,Q,customerPlan,vehiclePlan,customerDemand)
